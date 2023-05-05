61d6db10f6a8049ccf1fc60b0992dffc8b687e48
ELTON-GE <gexin7648@hotmail.com>
2023-05-05 14:32:37 +1000

    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Auth } from "./components/Auth";
 import { RoomLobby } from "./components/RoomLobby";
-import { useAppDispatch, useAppSelector } from "./redux/app/hooks";
+import { useAppSelector } from "./redux/app/hooks";
 import { Chat } from "./components/Chat";
 
 function App() {
   const isAuth = useAppSelector((state) => state.user.userIsAuth);
   const userRoom = useAppSelector((state) => state.user.userRoom);
-  const dispatch = useAppDispatch();
-  
+
   if (!isAuth) return <Auth />;
 
   if (isAuth && !userRoom) return <RoomLobby />;
